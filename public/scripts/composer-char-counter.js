2cd98a7e81fe3b7d94b5a38885644a959dba6617
Rafin Reza <rafintokyo@gmail.com>
2021-01-28 16:14:22 +0000

    add character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,33 +1,13 @@
-$(document).ready(function() {
-  $('#tweet-text').on("keydown", function(event) {
-    let counter = parseInt($('.counter').text());
-    // backspace has been pressed
-    if (event.which === 8) {
-      // don't increment it when it's 140 or over
-      if (counter < 140) {
-        counter++;
-        $('.counter').text(counter);
-      }
-    } else {
-      // here, don't decrement the counter if it's less than 0:
-      if (counter > 0) {
-        counter--;
-        $('.counter').text(counter);
-      }
-
-    }
-
-    // we'll toggle the caution here - the caution class will turn on
-    // when counter is equal to zero and toggle off otherwise. It just
-    // turns the text on to warn the user of the tweet length being at its limit.
-    if (counter === 0) {
-      $('.counter').addClass('counter-caution');
-    } else {
-      $('.counter').removeClass('counter-caution');
-    }
-  });
-
-  $('#tweet-submit').click(function() {
-    console.log(this);
+$(document).ready(function () {
+  // select textaera in class "new-tweet"
+  $(".new-tweet textarea").on("keyup", function (event) {
+    const countMax = 140;
+    const twUp = $(this).val().length;
+    const Tdown = countMax - twUp
+    $(".counter").text(Tdown);
+    // if text is greater than character count, turn red
+    if (Tdown <= 0) {
+      $(".counter").css("color", "red");
+    } 
   });
 });
