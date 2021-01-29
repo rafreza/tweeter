4ff2444f6e8fab27c420cba9bc9b33e4202b0d8d
Rafin Reza <rafintokyo@gmail.com>
2021-01-29 02:28:22 +0000

    Add comments, name author in package.json

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,7 @@
 
  
 
-
+//Front-end JQuery render 
   const createTweetElement = function (tweet) {
     let $article = $('<article>');
     let $header = $('<header>');
@@ -39,9 +39,10 @@
 
     $hr.addClass('line').appendTo($div);
 
+    // creates proper timestamp of when the tweet is created
     let createdTime = new Date(tweet['created_at']).toString().slice(4, 24);
-    $('<p>').addClass('footerText').text(createdTime).prependTo($footer);
     
+    $('<p>').addClass('footerText').text(createdTime).prependTo($footer);
     $footer.append($fontAwsomeIcons);
     $footer.appendTo($div);
     $div.addClass(tweet.user.name);
@@ -50,7 +51,7 @@
     return $div;
 
   };
-
+  //loops through tweet data array and render it to the tweetContainer
   const renderTweets = function (tweets) {
     for (const tweet of tweets) {
       console.log(tweet);
@@ -59,7 +60,7 @@
     }
   };
 
-    //render the new added elements
+  //render added tweets which is the last element of the data array
   const loadTweets = () => {
     $.get('/tweets')
       .then(data => {
@@ -68,15 +69,16 @@
   };
 
   
-
+  //main function
   $(document).ready(() => {
+  
 
   const $errorBox = $('<p>').addClass('error').text("Empty tweets! You didn't input anything!");
   $errorBox.prependTo($('.container')).hide();
   const $errorBox2 = $('<p>').addClass('error').text('Tweets are too long (more than 140 characters)!');
   $errorBox2.prependTo($('.container')).hide();
 
-    //arrow redirects to copse tweet
+    //arrow redirects to text box for new tweet
     $('.fa-angle-double-down').on('click', (event) => {
       $("#new-tweet-input").focus();
     });
@@ -87,13 +89,13 @@
    $('form').on('submit', (event) => {
     event.preventDefault();
     if ($('textarea').val().length === 0) {
-      $errorBox.slideDown();
-      $errorBox2.hide(); 
+      $errorBox.slideDown(); //Pops up an error message if no text is submitted to form
+      $errorBox2.hide(); //Hides any existing error prompts
     } else if ($('textarea').val().length > 140) {
-      $errorBox2.slideDown();
+      $errorBox2.slideDown(); //Pops up an error if tweet exceeds character count
       $errorBox.hide(); 
     } else {
-      $errorBox.slideUp();
+      $errorBox.slideUp(); //Clears all error messages if subsequent tweet meets criteria
       $errorBox2.slideUp();
       $.post('/tweets', $('form').serialize())
         .then(() => {
@@ -105,7 +107,7 @@
     }
   });
     
-      //initial render
+      //Renders the tweets
       $.get('/tweets')
         .then((data) => {
           renderTweets(data);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
-  // select textaera in class "new-tweet"
+  
   let num = 0;
  $('textarea').on('input', function () {
     num = $(this).val().length;
+    //Continues to reduce counter value while its value is greater than 0
     $(this).closest('section').find('span').html(`${140 - num}`).removeClass('warning');
+    //Counter will change colour to red indicating that the tweet exceeds 140 characters.
     if (num > 140) {
     $(this).closest('section').find('span').addClass('warning');
     }
